c4c20e0b605f0bcdd271bee87b3c8b9a0fee38e6
mohamed ahmed Krichen <57727551+medahmedkrichen@users.noreply.github.com>
2024-12-04 12:22:39 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -10,15 +10,11 @@ def install_if_not_installed(import_name, install_command):
         os.system(f"{install_command}")
 
 install_if_not_installed('openai-whisper', 'pip install openai-whisper==20240930')
-
-install_if_not_installed('TTS', 'pip install TTS==0.21.0')
-
 install_if_not_installed('pyannote.audio', 'pip install pyannote.audio==3.3.2')
-
 install_if_not_installed('deepface', 'pip install deepface==0.0.93')
 install_if_not_installed('librosa', 'pip install librosa==0.9.1')
-
 install_if_not_installed('packaging', 'pip install packaging==20.9')
+install_if_not_installed('TTS', 'pip install TTS==0.21.0')
 
 from IPython.display import HTML, Audio
 from base64 import b64decode
