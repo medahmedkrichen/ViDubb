3da0cc4d538f8bbe8a669b9711a5402bc25bcc29
mohamed ahmed Krichen <57727551+medahmedkrichen@users.noreply.github.com>
2024-12-04 12:10:46 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -7,7 +7,7 @@ def install_if_not_installed(import_name, install_command):
     try:
         __import__(import_name)
     except ImportError:
-        os.system(f"{install_command} > /dev/null 2>&1")
+        os.system(f"{install_command}")
 
 install_if_not_installed('TTS', 'pip install TTS==0.21.0')
 
