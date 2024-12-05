2d818f4c5b46f32e9cecbce352113b4226976d1f
mohamed ahmed Krichen <57727551+medahmedkrichen@users.noreply.github.com>
2024-12-05 22:31:11 +0100

    Update utils.py

--- tools/utils.py
+++ tools/utils.py
@@ -145,7 +145,7 @@ def cosine_similarity(embedding1, embedding2):
                 return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
 
 
-def extract_and_save_most_common_face(folder_path, threshold=0.3):
+def extract_and_save_most_common_face(folder_path, threshold=0.2):
                 """
                 Extracts and saves the most common face from the folder, saving it as 'max_image.jpg'.
                 """
