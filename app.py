b544b90c6f2332c2b90ad7a4ffefbc813ed4d9d6
mohamed ahmed Krichen <57727551+medahmedkrichen@users.noreply.github.com>
2024-12-15 02:29:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -361,15 +361,13 @@ class VideoDubbing:
             model_name = f"Helsinki-NLP/opus-mt-trk-{self.target_language}"
         elif self.target_language == 'tr':
             model_name = f"Helsinki-NLP/opus-mt-{self.source_language}-trk"
-        else:
-            model_name = f"Helsinki-NLP/opus-mt-{self.source_language}-{self.target_language}"
-
-        if self.source_language == 'zh-cn':
+        elif self.source_language == 'zh-cn':
             model_name = f"Helsinki-NLP/opus-mt-zh-{self.target_language}"
         elif self.target_language == 'zh-cn':
             model_name = f"Helsinki-NLP/opus-mt-{self.source_language}-zh"
         else:
             model_name = f"Helsinki-NLP/opus-mt-{self.source_language}-{self.target_language}"
+
 		
         tokenizer = MarianTokenizer.from_pretrained(model_name)
         model = MarianMTModel.from_pretrained(model_name).to(device)
